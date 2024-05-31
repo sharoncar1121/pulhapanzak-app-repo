e1f9dda7e39e15c1c27d5ae70c1e6b4ad31075ff
sharoncar1121 <109883948+sharoncar1121@users.noreply.github.com>
2024-05-31 11:25:33 -0600

    borrando location

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -20,6 +23,22 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"pulha-app-28338","appId":"1:503788973787:web:e0b7eba87be907c6cda6fe","storageBucket":"pulha-app-28338.appspot.com","locationId":"us-central","apiKey":"AIzaSyCycGdFt3pLgqOdafW7fHJ0dZh-U5jEviQ","authDomain":"pulha-app-28338.firebaseapp.com","messagingSenderId":"503788973787"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideMessaging(() => getMessaging())), importProvidersFrom(provideStorage(() => getStorage())),
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'pulha-app-28338',
+          appId: '1:503788973787:web:e0b7eba87be907c6cda6fe',
+          storageBucket: 'pulha-app-28338.appspot.com',
+          apiKey: 'AIzaSyCycGdFt3pLgqOdafW7fHJ0dZh-U5jEviQ',
+          authDomain: 'pulha-app-28338.firebaseapp.com',
+          messagingSenderId: '503788973787',
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideMessaging(() => getMessaging())),
+    importProvidersFrom(provideStorage(() => getStorage())),
   ],
 });
