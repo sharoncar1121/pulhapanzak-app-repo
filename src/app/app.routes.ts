ed392f9e5982159152391132458c7aad290ae9db
sharoncar1121 <109883948+sharoncar1121@users.noreply.github.com>
2024-06-05 00:41:01 -0600

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
-    path: '',
+    path: 'login',
     redirectTo: 'login',
     pathMatch: 'full',
   },
